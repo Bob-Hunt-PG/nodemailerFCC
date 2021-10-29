74a49d7476fb5305cec42559680897051a5ffc7d
Bob Hunt <85318042+Bob-Hunt@users.noreply.github.com>
2021-10-29 18:10:04 -0400

    completed the tutorial.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
@@ -11,7 +11,7 @@ app.listen(port, () => {
 
 
 
-// ** Transporter ** //
+//  Transporter
 // Pay attention, as apart from the user and the pass keys, 
 // which are your own credentials for your gmail account, 
 // the other three keys need to be retrieved after setting up OAuth.
@@ -27,3 +27,19 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+//  MailOptions Object 
+let mailOptions = {
+    from: codebuilder.bob@gmail.com,
+    to: codebuilder.bob@gmail.com,
+    subject: "nodemailerFFC Project",
+    text: "Hi from your nodemailer project"
+};
+
+// sendMail
+transporter.sendMail(mailOptions, function(err, data){
+    if (err) {
+        console.log("Error " + err);
+    }else{
+        console.log("Email sent successfully");
+    }
+});
